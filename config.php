f619959e92fab5060d02b0018d9d6fa325f8bbb4
zhiyu0617 <134248550+zhiyu0617@users.noreply.github.com>
2024-04-04 12:07:47 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,13 @@
+<?php
+    define('HOSTNAME', 'localhost');
+    define('USERNAME', 'root');
+    define('PASSWORD', '');
+    define('DB_NAME', 'kidsGames');
+    
+    $connection = new mysqli(HOSTNAME, USERNAME, PASSWORD, DB_NAME);
+
+    if ($connection->connect_error) 
+    {
+        die("Connection failed: " . $connection->connect_error);
+    }
 
