2c2d49dbfab7f3b6ce86d578b1c5faf849cf094c
JunchengHao <156448156+JunchengHao@users.noreply.github.com>
2024-04-05 10:02:39 -0400

    Update database.php

--- db/database.php
+++ db/database.php
@@ -1,1 +1,18 @@
+<?php
+// Database connection settings
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'kidsGames');
+
+function connectToDB() {
+    try {
+        $connection = new PDO("mysql:host=" . HOSTNAME . ";dbname=" . DATABASE, USERNAME, PASSWORD);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $connection;
+    } catch (PDOException $error) {
+        die("Connection to MySQL failed! <br>" . $error->getMessage());
+    }
+}
+?>
 
